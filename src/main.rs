4ed28dca7dfa3abd1375d77826bfb325371d23bf
Koushik <koushikhr1441@gmail.com>
2023-12-29 04:05:26 +0530

    prints panic messages when we panic

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic (_info: &PanicInfo) -> ! {
+fn panic (info: &PanicInfo) -> ! {
+    println !("{info}");
+
     loop {}
 }
 
@@ -14,10 +16,8 @@ static HELLO: &[u8] = b"Hello World";
 
 #[no_mangle]
 pub extern "C" fn _start () -> ! {
-    use core::fmt::Write;
-
-    vga_buffer::WRITER.lock ().write_str ("Hello again").unwrap ();
-    write !(vga_buffer::WRITER.lock (), ", some numbers: {} {}", 42, 1.337).unwrap ();
+    println !("Hello World {}", "!");
+    panic !("Some panic message");
 
     loop {}
 }
