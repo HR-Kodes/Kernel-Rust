0d236ec3c30febe31f4467f4dffc730eca90d36a
Koushik <koushikhr1441@gmail.com>
2024-01-03 01:46:58 +0530

    Integrations tests running

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,28 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(my_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-mod serial;
-mod vga_buffer;
-
 use core::panic::PanicInfo;
+#[allow(unused_imports)]
+use my_os::println;
 
-pub trait Testable {
-    fn run(&self);
-}
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello, world");
+    #[cfg(test)]
+    test_main();
 
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
+    #[allow(clippy::empty_loop)]
+    loop {}
 }
 
 // Panic handler other than test modes
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{info}");
+    println!("{}", info);
 
     loop {}
 }
@@ -53,34 +31,5 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-
-    loop {}
-}
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    #[cfg(test)]
-    test_main();
-
-    #[allow(clippy::empty_loop)]
-    loop {}
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-
-    for test in tests {
-        test.run();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+    my_os::test_panic_handler(info)
 }
